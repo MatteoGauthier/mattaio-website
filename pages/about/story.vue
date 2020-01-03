9cccb02367a2b9cc18101276de92525c0c7505a3
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-01-03 13:41:54 +0100

    Fix orthograph issue

--- pages/about/story.vue
+++ pages/about/story.vue
@@ -2,21 +2,21 @@
   <div class="root">
     <div v-if="$mq == 'laptop'" class="box-wsh content">
       <h2>Commencement</h2>
-      <p>Depuis mes 13 ans je m'interresse à l'informatique et à son fonctionnement, c'est à partir de la quatrième que je commence à découvrir le monde de la programmation (une infime partie bien sûr) c'est avec Visual Basic .NET que je commence la création de petites applications, la première fut un utilitaire de 3 Forms permettant de gérer la veille de l'appareil. C'était très simple mais cette approche visuel de la programmation me plaisais (l'interface graphique click & drag des composants rendait la création d'interface simplifié). Au fil du temps j'ai appris différentes notions d'informatiques, de réseaux, de design. C'est en 3ème que j'ai suivi mon premier cours de programmation en ligne (MOOC) avec les PDFs du site du zéro renommé OpenClassrooms aujourd'hui, vous pouvez d'ailleurs retrouver les cours que j'ai validés dans la section cours de mon site web. OpenClassrooms est un très bon moyen d'enrichir ses connaissances gratuitement c'est vraiment pour moi le point de départ d'un apprentissage, le cours "Apprenez à coder avec JavaScript" m'a orienté vers l'apprentissage développement web pour à terme espérer devenir développeur web (je suis encore loin mais je fais le maximum pour atteindre mon objectif).</p>
+      <p>Depuis mes 13 ans je m'interesse à l'informatique et à son fonctionnement, c'est à partir de la quatrième que je commence à découvrir le monde de la programmation (une infime partie bien sûr) c'est avec Visual Basic .NET que je commence la création de petites applications, la première fut un utilitaire de 3 Forms permettant de gérer la veille de l'appareil. C'était très simple mais cette approche visuelle de la programmation me plaisait (l'interface graphique click & drag des composants rendait la création d'interface simplifiée). Au fil du temps j'ai appris différentes notions d'informatique, de réseaux, de design. C'est en 3ème que j'ai suivi mon premier cours de programmation en ligne (MOOC) avec les PDFs du site du zéro renommé OpenClassrooms aujourd'hui, vous pouvez d'ailleurs retrouver les cours que j'ai validés dans la section cours de mon site web. OpenClassrooms est un très bon moyen d'enrichir ses connaissances gratuitement c'est vraiment pour moi le point de départ d'un apprentissage, le cours "Apprenez à coder avec JavaScript" m'a orienté vers l'apprentissage développement web pour à terme espérer devenir développeur web (je suis encore loin mais je fais le maximum pour atteindre mon objectif).</p>
 
       <h2>Février 2017</h2>
-      <p>Pendant leurs scolarité tous les élèves ont été convier à effectués un stage de 3ème leurs permettant peut-être d’orienter leur projet professionnel, pour ma pars c'est dans les locaux de l'université de Poitiers que j'ai eu la chance de faire mon stage de 3ème, Lionel Lombardi mon maître de stage responsable informatique du département SFA de l'université. Ce stage c'est très bien passé j'ai découvert le fonctionnement d'une équipe technique, la résolution de problème réseaux, le quotidien d'un technicien informatique.</p>
+      <p>Pendant leur scolarité tous les élèves ont été conviés à effectuer un stage de 3ème leur permettant peut-être d’orienter leur projet professionnel, pour ma part c'est dans les locaux de l'université de Poitiers que j'ai eu la chance de faire mon stage de 3ème, Lionel Lombardi mon maître de stage responsable informatique de la faculté SFA de l'université. Ce stage s'est très bien passé, j'ai découvert le fonctionnement d'une équipe technique, la résolution de problèmes réseaux, le quotidien d'un technicien informatique.</p>
       <h2>Été 2017</h2>
-      <p>Durant cet été j'ai consacré beaucoup de temps à l'apprentissage des languages du Web à travers beaucoup d'expérimentations, d'exploration de documentation, de création de page web sommaire. Je me suis aussi servis de cours en ligne (liens disponibles dans la section cours). J'ai directement utilisé les connaissances que je venais d'apprendre pour commencer à élaborer mon premier site web nommé semoule.fr (oui oui semoule.fr), pas de but particulier quand à la création de ce site mise à part les connaissances que cela m'a apportés (notamment la découverte des technologies sous le fonctionnement du web, comme les DNS, le crawling, SSH, FTP, WebDAV, APIs, et d'autres protocoles..). Un petit projet web comme celui-ci m'a permis d'apprendre à maîtriser le logiciel git (logiciel de gestion de versions décentralisé), c'est pourquoi grace à l'influence de développeurs open-source j'ai décidé de publier publiquement le code de tout mes projets sur la plateforme Github pour permettre à d'autres de faciliter leurs apprentissages, c'est avec ce schéma que j'ai découvert la programmation).</p>
+      <p>Durant cet été j'ai consacré beaucoup de temps à l'apprentissage des languages du Web à travers beaucoup d'expérimentations, d'exploration de documentation, de création de page web sommaire. Je me suis aussi servis de cours en ligne (liens disponibles dans la section cours). J'ai directement utilisé les connaissances que je venais d'apprendre pour commencer à élaborer mon premier site web nommé semoule.fr (oui oui semoule.fr), pas de but particulier quand à la création de ce site mis à part les connaissances que cela m'a apporté (notamment la découverte des technologies sur le fonctionnement du web, comme les DNS, le crawling, SSH, FTP, WebDAV, APIs, et d'autres protocoles..). Un petit projet web comme celui-ci m'a permis d'apprendre à maîtriser le logiciel git (logiciel de gestion de versions décentralisé), c'est pourquoi grâce à l'influence de développeurs open-source j'ai décidé de publier publiquement le code de tous mes projets sur la plateforme Github pour permettre à d'autres de faciliter leurs apprentissages, c'est avec ce schéma que j'ai découvert la programmation).</p>
       <details>
-        <summary>Plus d'info sur semoule.fr et les compétences travaillés durant cette périodes</summary>
+        <summary>Plus d'info sur semoule.fr et les compétences travaillées durant cette période</summary>
         <div class="pad-details">
-          <p>Nommé Semoule.fr en Août 2017 j'ai décidé de me lancer dans la création d'un site web me permettant d'y introduire différents projets que je dirigent (à 16 ans je n'ai évidemment pas de projet imposant, ce ne sont que des applications, des article, des créations "numérique"). Ce site me permets de m'améliorer en programmation web, mais aussi en programmation back-end où j'effectue quelques manipulation des logiciels Apache, nginx ou encore NodeJS.</p>
-          <h3>Je peux donc effectuer :</h3>
+          <p>Nommé Semoule.fr en Août 2017 j'ai décidé de me lancer dans la création d'un site web me permettant d'y introduire différents projets que je dirige (à 16 ans je n'ai évidemment pas de projets de grande ampleur, ce ne sont que des applications, des articles, des créations "numériques"). Ce site me permet de m'améliorer en programmation Front-end, mais aussi en programmation back-end où j'effectue quelques manipulations des logiciels Apache, nginx ou encore NodeJS.</p>
+          <h3>Je peux donc effectuer en août 2017 :</h3>
           <ul>
-            <li>La création d'un site simple avec les technologies Web (HTML, CSS, JS) et avec potentiellement des framework tels que jQuery, VueJS ou autres.. Je ne peux pas affirmé avoir l'appétence de fournir la construction complète d'un site Internet, mais pour vous donnez un ordre d'idée voici un registre des certificats informatiques que j'ai réussi (encore une fois cela n'est pas significatif, les certificats peuvent provenir de site de formations informatiques tels que OpenClassroom.com ou Grafikart.fr). Ces certificats de réussite de formations sont donc disponibles à adresse semoule.fr/skills</li>
+            <li>La création d'un site simple avec les technologies Web (HTML, CSS, JS) et avec potentiellement des frameworks tels que jQuery, VueJS ou autres.. Je ne peux pas affirmer avoir l'aptitude de fournir la construction complète d'un site Internet, mais pour vous donner un ordre d'idée voici un registre des certificats informatiques que j'ai réussis (les certificats peuvent provenir de site de formations informatiques tels que OpenClassroom.com ou Grafikart.fr). Ces certificats de réussite de formations sont donc disponibles à adresse mattaio.netlify.com/about/courses</li>
             <li>La manipulation basique d'un serveur apache, comme par exemple la configuration de l'accès à un serveur (htaccess, compression). Je sais me repérer dans un environnement serveur apache.</li>
-            <li>Utilisation complète d'un CMS comme WordPress, Joomla!, Drupal. En général je construis une application sans CMS pour préserver le côté lightweight et fais mains d'un site web. Semoule.fr ne dispose pas de CMS, même si l'utilisation d'un CMS peut être très utile pour un projet web.</li>
+            <li>Utilisation complète d'un CMS comme WordPress, Joomla!, Drupal. En général je construis une application sans CMS pour préserver le côté lightweight et le côté "fait mains" d'un site web. Semoule.fr ne dispose pas de CMS, même si l'utilisation d'un CMS peut être très utile pour un projet web.</li>
           </ul>
         </div>
       </details>
@@ -24,23 +24,24 @@
       <details>
         <summary>Plus d'info sur mon apprentissage de Python en 2017</summary>
         <div>
-          <p>Je suis en apprentissage de ce language car je le trouve agréable au développement et compréhensible rapidement. Il offre aussi des fonctionnalités fa ciles d'utilisation et universelle. Le langage Python ma d'abord permis de créer des "scripts" d'exécution linéaire (macros), mais je me suis ensuite tourné sur son utilisation comme applications CLI (Command Line).</p>
-          <h3>Je peux donc effectuer :</h3>
+          <p>Je suis en apprentissage de ce language car je le trouve agréable au développement et compréhensible rapidement. Il offre aussi des fonctionnalités faciles d'utilisation et universelles. Le langage Python m'a d'abord permis de créer des "scripts" d'exécution linéaire (macros), mais je me suis ensuite tourné sur son utilisation comme applications CLI (Command Line).</p>
+          <h3>Je peux donc effectuer en 2017 :</h3>
           <ul>
-            <li>L'organisation d'une application Python avec l'utilisation de fonctions basique. L'intégration d'un algorigramme dans un programme mais familière.</li>
+            <li>L'organisation d'une application Python avec l'utilisation de fonctions basiques. L'intégration d'un algorigramme dans un programme m'est familière.</li>
             <li>La manipulation de modules pour enrichir mon code.</li>
             <li>Des calculs mathématiques de niveau scientifique en lycée.</li>
           </ul>
-          <p>Python n'ai pas un language que j'utilise souvent, mais il fait parti des languages que je souhaite maîtrisé.</p>
+          <p>Python n'est pas un language que j'utilise souvent, mais il fait partie des languages que je souhaite maîtriser.</p>
         </div>
       </details>
       <h2>Année de seconde 2017-2018</h2>
       <p>
-        Pour mon année de seconde j'ai choisi de prendre l'option ICN (Informatique et Création Numérique), cette option ne m'a pas était très utile mais elle ma permis de travailler en parallèle du cours sur d'autre projets de développement perso (tels que l'apprentissage de la programmation Javascript server-side avec NodeJS, le développement en Ruby, la découverte de ThreeJS).
-        <br />Cette année de seconde à été productive aussi bien pour ma scolarité que pour mes compétences en informatiques, je me suis beaucoup intéressé cette année aux cryptomonnaies, à la blockchain et à son fonctionnement.
+        Pour mon année de seconde j'ai choisi de prendre l'option ICN (Informatique et Création Numérique), cette option ne m'a pas été très utile mais elle m'a permis de travailler en parallèle du cours sur d'autres projets de développement perso (tels que l'apprentissage de la programmation Javascript server-side avec NodeJS, le développement en Ruby, la découverte de ThreeJS).
+        <br />Cette année de seconde a été productive aussi bien pour ma scolarité que pour mes compétences en informatique, je me suis beaucoup intéressé cette année aux cryptomonnaies, à la blockchain et à son fonctionnement.
       </p>
       <h2>Année de première 2018-2019</h2>
-      <p>Définitivement l'année où j'ai appris le plus de choses, cette année de première à été très active poussé par l'objectif de fin d'année, un stage dans l'entreprise TBWA l'une des plus grosses entreprises de publicités de France. La première scientifique à été difficile pour moi, palier à mes difficultés scolaires m'a pris beaucoup de temps, malgré cela j'ai appris beaucoup cette année. J'ai consacré beaucoup de temps à l'apprentissage du framework Front-End VueJS ⏱️⏱️⏱️</p>
+      <p>Définitivement l'année où j'ai appris le plus de choses, cette année de première a été très active poussé par l'objectif de fin d'année, un stage dans l'entreprise TBWA\Paris l'une des plus grosses entreprises de publicités de France. La première scientifique a été difficile pour moi, palier mes difficultés scolaires m'a pris beaucoup de temps, malgré cela j'ai appris beaucoup cette année. J'ai consacré beaucoup de temps à l'apprentissage du framework Front-End VueJS
+        <br><br> <i>en cours d'écriture</i></p>
     </div>
 
   </div>
@@ -73,7 +74,7 @@ data: function() {
     getWindowWidth(event) {
         this.windowWidth = document.documentElement.clientWidth;
         console.log(this.$mq)
-      },  
+      },
 
       getWindowHeight(event) {
         this.windowHeight = document.documentElement.clientHeight;
