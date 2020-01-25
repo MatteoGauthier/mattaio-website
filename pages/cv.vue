c042a7d7b967ed244381c7707ec4e97b88437478
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-01-25 23:30:07 +0100

    Adding my cv

--- pages/cv.vue
+++ pages/cv.vue
@@ -7,7 +7,7 @@
 export default {
 
   mounted() {
-    const url = "https://xn--matto-7ra.dev/CV_MattèoGauthier_2019.pdf";
+    const url = "https://xn--matto-7ra.dev/CV_MattèoGauthier_2020_2.0.pdf";
     window.open(url, "_newtab");
  window.history.back(0)
   }
