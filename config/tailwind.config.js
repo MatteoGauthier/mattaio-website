1f05d4e7ea268376e59ac8fb3755f07b505c28ee
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-02-16 18:32:34 +0100

    resolving path error with tailwindcss

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,15 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
-}
+  theme: { darkSelector: ".mode-dark" },
+  variants: {
+    backgroundColor: ["dark", "dark-hover", "dark-group-hover"],
+    borderColor: ["dark", "dark-focus", "dark-focus-within"],
+    textColor: ["dark", "dark-hover", "dark-active"]
+  },
+  plugins: [require("tailwindcss-dark-mode")()]
+};
