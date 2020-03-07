a13689dfae3de659ee6c2f579e1758a84671494d
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-03-07 23:44:38 +0100

    yarn create

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,72 +1,13 @@
 <template>
-  <div class="page-container">
-    <TheNavbar/>
-    <nuxt/>
-
-    <Footer/>
+  <div>
+    <nuxt />
   </div>
 </template>
 
-<script>
-import TheNavbar from "@/components/TheNavbar";
-import Footer from "@/components/Footer.vue";
-import tippy from "tippy.js";
-
-export default {
-  components: {
-    TheNavbar,
-    Footer
-  },
-  mounted() {
-    var soonPop = document.querySelectorAll(".soonPop");
-    tippy(soonPop, {
-      content: "Soon",
-      placement: "bottom",
-      theme: "light-border"
-    });
-    
-    var enTravaux = document.querySelectorAll(".enTravaux");
-    tippy(enTravaux, {
-      content: "En travaux",
-      placement: "left",
-      theme: "light-border"
-    });
-    
-    }
-};
-</script>
-
-<style lang="scss">
-
-footer {
-    display: flow-root;
-    padding: 3rem 1.5rem 3rem !important; // TODO I don't know how to override the bulma default value
-  }
-html, body {
-  height: 100%;
-}
-body {
-  display: flex;
-  flex-direction: column;
-}
-.content {
-  flex: 1 0 auto;
-}
-.footer {
-  flex-shrink: 0;
-}
-
-.page-container {
-  position: relative;
-  min-height: 100vh;
-}
-
-nuxt {
-  padding-bottom: 2.5rem;
-}
+<style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
