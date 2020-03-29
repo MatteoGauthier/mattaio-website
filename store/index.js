4047871fdd6cbf3d631682fe38cd4fbd7e703d44
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-03-29 16:35:36 +0200

    testinggggg

--- store/index.js
+++ store/index.js
@@ -14,7 +14,7 @@ export const actions = {
   async nuxtServerInit({ commit }) {
     const fm = require("front-matter");
     let files = await require.context(
-      "~/assets/content/blog/",
+      "~/content/blog/",
       false,
       /\.md$/
     );
