e198fedc0a4e540727c2e6b3bee746e47d54c62f
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-04-02 18:38:24 +0200

    Trying to resolve adblock issue

--- pages/Portfolio.vue
+++ pages/Portfolio.vue
@@ -1,48 +1,46 @@
 <template>
   <div>
     <client-only>
-
-
-    <CoolLightBox :items="images" :index="index" loop @close="index = null"></CoolLightBox>
+      <CoolLightBox :items="images" :index="index" loop @close="index = null"></CoolLightBox>
     </client-only>
     <div class="inline-flex flex-wrap">
-      <img class="flex-1 object-cover h-64 m-2 shadow rounded max-w-md transition-all duration-700" v-for="(src,index) in images" :key="index" @click="setIndex(index)" v-lazy="src" alt />
+      <img class="flex-1 object-cover h-64 max-w-md m-2 transition-all duration-700 rounded shadow" v-for="(src, index) in images" :key="index" @click="setIndex(index)" v-lazy="src" alt />
     </div>
   </div>
 </template>
 
 <script>
-import CoolLightBox from 'vue-cool-lightbox'
-import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
-
+import CoolLightBox from "vue-cool-lightbox";
+import "vue-cool-lightbox/dist/vue-cool-lightbox.min.css";
 
 export default {
-components: {
+  components: {
     CoolLightBox
   },
   data() {
     return {
       images: [
-        'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Home Mattèo.dev 2020.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Blog Mattèo.dev 2020.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Portfolio Mattèo.dev 2020.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/CV_MattèoGauthier_2020_2.1.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Dpliance Dashboard Mockup.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Editing Poitiers Home Page.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Dpliance WebSite.png',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Mattèo.dev Home Black.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Mattèo.dev Home White.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/MiniCard.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Proto Cross De ligugé Mobile.jpg',
-  'https://cdn.jsdelivr.net./gh/MattixNow/StaticHost@master/portfolio/Spiaggia HomePage.jpg'
-],
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Home Mattèo.dev 2020.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Blog Mattèo.dev 2020.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Portfolio Mattèo.dev 2020.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/CV_MattèoGauthier_2020_2.1.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Dpliance Dashboard Mockup.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Editing Poitiers Home Page.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Dpliance WebSite.png",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Mattèo.dev Home Black.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Mattèo.dev Home White.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/MiniCard.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Proto Cross De ligugé Mobile.jpg",
+        "https://cdn.jsdelivr.net/gh/MattixNow/StaticHost@master/portfolio/Spiaggia HomePage.jpg"
+      ],
       index: null
-    }
+    };
   },
   methods: {
     setIndex(index) {
-      this.index = index
-    }}
+      this.index = index;
+    }
+  }
 };
 </script>
 
