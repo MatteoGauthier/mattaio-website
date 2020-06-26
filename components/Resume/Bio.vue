6902612efcd91d1a7aa1a4f777233756d38c39b7
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-06-26 19:17:33 +0200

    adding vercel redirections

--- components/Resume/Bio.vue
+++ components/Resume/Bio.vue
@@ -2,7 +2,7 @@
   <fragment>
     <div class="pb-2 font-serif leading-8 sm:w-1/4 text-xxl sm:text-right sm:pr-8 sm:pb-0">Bio</div>
     <div class="sm:w-2/4 more">Salut je m’appelle Mattèo Gauthier, j’ai 17 ans et je suis développeur web novice. Actuellement au Lycée Nelson Mandela en Terminal S SI.</div>
-    <a href="/CV" class="flex items-center justify-center mt-2 sm:block sm:w-1/4 side sm:mt-0">
+    <a href="https://mattèo.dev/CV" class="flex items-center justify-center mt-2 sm:block sm:w-1/4 side sm:mt-0">
       <img src="~/assets/images/CVPreview.jpg" alt="Curriculum Vitae / CV" class="block w-16 mr-4 border border-gray-300 rounded sm:hidden md:block sm:mr-0 sm:absolute sm:w-1/5 lg:w-56 sm:ml-8 mg-cv-img" />
       <span class="hidden text-gray-800 sm:inline-block">← Découvrir mon cv</span>
     </a>
